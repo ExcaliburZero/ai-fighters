4ffbb878ca4869a97eb62d1934e5894ce0189806
Christopher Wells <cwellsny@nycap.rr.com>
2018-01-05 15:59:19 -0500

    Create from project template

---
+++ desktop/src/com/ai/fighters/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ai.fighters.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ai.fighters.AIFighters;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AIFighters(), config);
+	}
+}
